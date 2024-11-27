4b007ff3993d2eab9f7100a64ac0b6e76635d402
gordon-lim <gordonlim214@gmail.com>
2024-11-26 21:28:55 -0800

    added/renamed scripts to download asvspoof19 LA & 21 DF

---
+++ ASVspoof2021/download_asvspoof2019_la.sh
@@ -0,0 +1,4 @@
+wget https://datashare.ed.ac.uk/bitstream/handle/10283/3336/LA.zip
+unzip LA.zip
+mv LA/* data/
+rm -r LA.zip LA
--- ASVspoof2021/download_asvspoof2021_df.sh
+++ ASVspoof2021/download_asvspoof2021_df.sh
@@ -1,8 +1,8 @@
 wget -O asvspoof2021-df-data.zip https://zenodo.org/api/records/4835108/files-archive
-mkdir -p ASVspoof2021/data
-unzip asvspoof2021-df-data.zip -d ASVspoof2021/data
+mkdir data
+unzip asvspoof2021-df-data.zip -d data
 rm asvspoof2021-df-data.zip
-cd ASVspoof2021/data
+cd data
 for file in *.tar.gz; do
     tar -xvf "$file" -C ./
 done
